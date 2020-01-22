1701a1166e8f5578ee37c9254fce6863f17c7e42
christynv <vasquez.christyn@gmail.com>
2020-01-21 23:09:17 -0500

    Add recyclerView and getRecipeURL
    Add recipes to main menu with recyclerView and read recipe JSON

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.bakingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recipeList;
+    private LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recipeList = findViewById(R.id.recipe_card);
+        layoutManager = new LinearLayoutManager(this);
+        recipeList.setLayoutManager(layoutManager);
     }
 }
