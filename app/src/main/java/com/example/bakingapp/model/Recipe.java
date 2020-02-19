e910e34252ff56c330323aa257dfdc1ac4186bf7
christynv <vasquez.christyn@gmail.com>
2020-02-18 23:05:43 -0500

    Cleaned up Recipe Model

--- app/src/main/java/com/example/bakingapp/model/Recipe.java
+++ app/src/main/java/com/example/bakingapp/model/Recipe.java
@@ -7,19 +7,15 @@ public class Recipe {
     private int id;
     private String recipeName;
     private int servings;
-    private List<Ingredient> ingredients;
-    private List<Steps> steps;
 
     public Recipe() {
 
     }
 
-    public Recipe(int id, String recipeName, int servings, List<Ingredient> ingredients, List<Steps> steps) {
+    public Recipe(int id, String recipeName, int servings) {
         this.id = id;
         this.recipeName = recipeName;
         this.servings = servings;
-        this.ingredients = ingredients;
-        this.steps = steps;
     }
 
     public int getId() {return id;}
@@ -34,11 +30,4 @@ public class Recipe {
 
     public void setServings(int servings) {this.servings = servings;}
 
-    public List<Ingredient> getIngredients() {return ingredients;}
-
-    public void  setIngredients(List<Ingredient> ingredients) {this.ingredients = ingredients;}
-
-    public List<Steps> getSteps() {return steps;}
-
-    public void setSteps(List<Steps> steps) {this.steps = steps;}
 }
